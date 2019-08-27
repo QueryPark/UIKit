8dbcdcaf5b692d45d6298fce718dcfba82411f99
Jesse Liptak <jesse.liptak@gmail.com>
2019-08-27 15:42:47 -0600

    simplify build

--- packages/qp-react-ui/package-scripts.js
+++ packages/qp-react-ui/package-scripts.js
@@ -1,14 +1,12 @@
 const npsUtils = require('nps-utils')
 const series = npsUtils.series
 const rimraf = npsUtils.rimraf
-const concurrent = npsUtils.concurrent
 
 module.exports = {
   scripts: {
     clean: {
       default: series(
         rimraf('dist'),
-        rimraf('lib'),
         rimraf('docs/dist')
       )
     },
@@ -16,20 +14,13 @@ module.exports = {
       description: 'clean dist directory and run all builds',
       default: series(
         rimraf('dist'),
-        rimraf('lib'),
-        concurrent.nps('build.rollup', 'build.babel'),
-        rimraf('lib/__tests__')
+        'nps build.rollup'
       ),
-      rollup: 'rollup --config',
-      babel: 'babel src -d lib',
-      watch: 'babel src -d lib -w',
+      rollup: 'rollup --config ./rollup.config.js --environment BUILD:production',
       docs: series(rimraf('docs/dist'), 'webpack --progress -p')
     },
     publish: {
-      default: series(
-        'nps build.docs',
-        'nps build'
-      )
+      default: 'nps build'
     }
   }
 }
